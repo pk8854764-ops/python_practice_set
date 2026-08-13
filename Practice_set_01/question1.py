a27a636eb4324f6d88aa97cd4aa61368b829c3be
Pooja <pk8854764@gmail.com>
2026-08-13 22:15:31 +0530

    Add variables for personal information and print them

---
+++ Practice_set_01/question1.py
@@ -0,0 +1,9 @@
+# Q1. Create variables to store your name,age,city and salary,then print them.
+name = "Pooja"
+age = 19
+city = "Dehli"
+salary = 25000
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Salary:", salary)
