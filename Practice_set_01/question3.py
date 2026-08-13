24819797484637bd88f6896562e0e8355a86f2e3
Pooja <pk8854764@gmail.com>
2026-08-13 23:06:50 +0530

    Create question3.py

---
+++ Practice_set_01/question3.py
@@ -0,0 +1,5 @@
+# Q3. Accept two numbers from the user and print their sum.
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+sum = num1 + num2
+print("Sum =", sum)
