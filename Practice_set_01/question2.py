9eb5da92b8e6525ab2dc205509f898d4b39fe883
Pooja <pk8854764@gmail.com>
2026-08-13 22:39:22 +0530

    Create question2.py

---
+++ Practice_set_01/question2.py
@@ -0,0 +1,18 @@
+# Q2. Swap the value of two variable using both the traditional method and python's tuple unpacking.
+# Traditional method
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+print("After swapping using traditional method:")
+print("a =", a)
+print("b =", b)
+# Python's tuple unpaking 
+x = 30
+y = 40
+x, y = y, x
+print("After swapping using tuple unpacking:")
+print("x =", x)
+print("y =", y)
+
