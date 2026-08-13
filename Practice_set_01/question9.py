b0a53e447925db2d868d8a1a840e667528272029
Pooja <pk8854764@gmail.com>
2026-08-13 23:59:00 +0530

    Add program to display full name from first and last name
    Implemented a program to input and display full name.

---
+++ Practice_set_01/question9.py
@@ -0,0 +1,4 @@
+# Q9. Write a program to input the first name,last name separately and display the full name in a single line.
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+print("full name:",first_name,last_name")
