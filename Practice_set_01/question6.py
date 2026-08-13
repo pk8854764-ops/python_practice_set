e55048901aa4f7f721b57e787d4db9a3aa732993
Pooja <pk8854764@gmail.com>
2026-08-13 23:27:27 +0530

    Create question6.py

---
+++ Practice_set_01/question6.py
@@ -0,0 +1,6 @@
+# Q6. Assign the same value to three variables and verify the result.
+a = b = c = 100
+print("a =", a)
+print("b =", b)
+print("c =", c)
+print("All three variable have the same value:", a = = b = = c)
