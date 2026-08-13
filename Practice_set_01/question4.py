52db2180df6654f9df71190cc0981fe01c75ae88
Pooja <pk8854764@gmail.com>
2026-08-13 23:16:24 +0530

    Create question4.py

---
+++ Practice_set_01/question4.py
@@ -0,0 +1,9 @@
+# Q4. Create variables of type int,float,str,bool,and display their data types using type().
+integer_value = 10
+float_value = 10.5
+string_value = "python"
+boolean_value = True
+print("Integer:" , type(integer_value))
+print("Float:" , type(float_value))
+print("String:" , type(string_value))
+print("Boolean:" , type(boolean_value))
