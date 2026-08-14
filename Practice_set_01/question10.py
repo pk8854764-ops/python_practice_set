da9c78b481a7c2b5d42946a3ddf93846ebd6ed53
Pooja <pk8854764@gmail.com>
2026-08-14 22:42:01 +0530

    Create question10.py

---
+++ Practice_set_01/question10.py
@@ -0,0 +1,6 @@
+# Q10. Write a program to input an employee's name and monthly salary then display the annual salary.
+name = input("Enter employee's name: ")
+monthly_salary = float(input("Enter monthly_salary: "))
+annual_salary = monthly_salary*12
+print("Empolyee Name:", name)
+print("Annual salary:", annual_salary)
