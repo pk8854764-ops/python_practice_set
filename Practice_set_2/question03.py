df56f36ed3bcd5a7907a0e132334d501a620498b
Pooja <pk8854764@gmail.com>
2026-08-15 18:10:19 +0530

    Create question03.py

---
+++ Practice_set_2/question03.py
@@ -0,0 +1,9 @@
+# Q3. Write a program to input two numbers and check:Are the equal?,Are they not equal?,Is the first number greater than second?,Is the first number less than or equal to the second?.
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+print("Equal:",a==b)
+print("Not Equal:",a!=b)
+print("First greater than Second:",a>b)
+print("First less than or Equal to Second:",a<=b)
+
+
