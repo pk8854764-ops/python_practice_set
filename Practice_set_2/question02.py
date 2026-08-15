214c4ff169c417f1eb394706ac1e0b0e0552afcc
Pooja <pk8854764@gmail.com>
2026-08-15 17:56:24 +0530

    Create question02.py

---
+++ Practice_set_2/question02.py
@@ -0,0 +1,12 @@
+# Q2. Write a program that:Takes an integer as input,apply the following operation one by one:+=10,-=5,*=2,/=3.Disply the value after each operation.
+n = int(input("Enter an integer:"))
+n += 10
+print("After += 10:",n)
+n -= 5
+print("After -= 5:",n)
+n *= 2
+print("After *= 2:",n)
+n /= 3
+print("After /= 3:",n)
+
+      
